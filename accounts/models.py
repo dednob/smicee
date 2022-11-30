94fe574e3667116a8bf843ecfa92a2042a667962
Mostafa Musab Naveed <mustafanaveed404@gmail.com>
2022-11-30 13:20:21 +0600

    choices of role removed

--- accounts/models.py
+++ accounts/models.py
@@ -8,10 +8,6 @@ from django.contrib.auth.models import User
 
 
 class Account(models.Model):
-    choices = [
-        ('admin', 'Admin'),
-        ('employee', 'Employee'),
-        ('client', 'Client'),
-    ]
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     phone_no = models.CharField(max_length=15)
